1d4b34424805c562d5e82962d4aedcb13a4b6a6b
Nana Yaw <jeffrey-14@live.co.uk>
2025-07-14 13:21:39 -0400

    Initial Commit

---
+++ TriageEngineerTool/TriageEngineerToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriageEngineerToolApp.swift
+//  TriageEngineerTool
+//
+//  Created by Nana Yaw on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TriageEngineerToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
